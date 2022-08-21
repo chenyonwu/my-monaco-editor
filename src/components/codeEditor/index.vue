7539e357af7771b1de08875934832db7dd672d68
chenyonwu <1906167102@qq.com>
2022-08-21 15:49:48 +0800

    monaco-editor的demo实现成功

--- src/components/codeEditor/index.vue
+++ src/components/codeEditor/index.vue
@@ -1,5 +1,8 @@
 <template>
     hello, monaco-editor
+    <div ref="codeEditorRef" class="code-editor">
+
+    </div>
 </template>
 
 <script>
@@ -7,3 +10,29 @@
         name: 'CodeEditor',
     }
 </script>
+
+<script setup>
+    import * as monaco from 'monaco-editor';
+
+    const codeEditorRef = ref(null);
+    const monacoInstance = shallowRef(null);
+
+    onMounted(() => {
+        init();
+    })
+
+    function init() {
+        monacoInstance.value = monaco.editor.create(unref(codeEditorRef), {
+            value: ['function x() {', '\tconsole.log("Hello, world");', '}'].join('\n'),
+            language: "javascript",
+            theme: 'vs-dark',
+        });
+    }
+</script>
+
+<style lang="scss" scoped>
+    .code-editor {
+        width: 700px;
+        height: 700px;
+    }
+</style>
